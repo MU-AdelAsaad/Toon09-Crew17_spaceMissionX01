4e9c05f3767744d871f0ad4d073b0360ffe1aceb
MU-AdelAsaad <114301565+MU-AdelAsaad@users.noreply.github.com>
2023-03-23 13:58:01 +0100

    start

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.planets {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.apache.poi.poi;
-
-
-    opens com.example.planets to javafx.fxml;
-    exports com.example.planets;
-}
